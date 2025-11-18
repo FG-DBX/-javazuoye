703c3225d65cb05b9cb5a737c46718a1eb17f165
FG-DBX <3054136269@qq.com>
2025-11-19 03:15:48 +0800

    Delete zuoye/zy03 directory

--- zuoye/zy03/Veterinarian.java
+++
@@ -1,8 +0,0 @@
-package zy03;
-
-// Veterinarian.java
-public class Veterinarian {
-    void treatAnimal(Animal animal) {
-        System.out.println("Лечение животного. Корм: " + animal.food + ", Локация: " + animal.location);
-    }
-}
